54482b26dfc44c0c026fabc70bd1acdf610e0ff8
Satvik <satvikshashank11@gmail.com>
2024-11-29 10:18:25 +0530

    macos release

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,7 +3,8 @@ import FlutterMacOS
 
 @main
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
+    override func applicationDidFinishLaunching(_ aNotification: Notification) {
+        super.applicationDidFinishLaunching(aNotification)
+        // Custom setup can be done here
+    }
 }
